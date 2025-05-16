4efc129ddcff310aa78d38ceda26dcd88221258d
Lkyumin <kyumin4959@gmail.com>
2025-05-16 19:29:41 +0900

    Update apiLink.py

--- src/weather/apiLink.py
+++ src/weather/apiLink.py
@@ -11,10 +11,7 @@ def get_weather_manual( pos : Tuple[float,float], date : int, time : int
                        , data_type : str = 'JSON', day : int = 0 ):
     # key and url
     
-    base_dir = os.path.dirname(os.path.abspath(__file__))
-    key_file = os.path.join(base_dir, ".key", ".servicekey_decode")
-    with open(key_file,'r') as f:
-        service_key = f.read()
+    service_key = "S1BaX+xBN5BHD/KvjHaEAjAUw0cifZi2CdoDIeShd6bQ+EWcanogJq5s4v5t32PLhSdymB6jglQwMW9+8O1QFw=="
     url = "http://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/getVilageFcst"
  
     params = {
@@ -22,11 +19,12 @@ def get_weather_manual( pos : Tuple[float,float], date : int, time : int
         'numOfRows' : number_of_rows,
         'pageNo' : page_number,
         'dataType' : data_type,
-        'base_date' : date + day,
+        'base_date' : int(date) + day,
         'base_time' : time,
         'nx' : pos[0],
         'ny' : pos[1]
     }
+    print(params)
 
     return requests.get(url,params)
 
@@ -40,12 +38,27 @@ def get_weather_auto( day : int = 0 ):
     corrent_time = (date_int%10000)/100
     corrent_time = int(corrent_time - ((corrent_time-2)%3))%24
     corrent_time = f'{corrent_time:02d}00'
-    
+    corrent_date = date_int/10000
     data_type = "JSON"
 
     page_no = 1
-    return get_weather_manual( pos, date_int, corrent_time )
+    return get_weather_manual( pos, corrent_date, corrent_time )
+
+def response_to_json(response):
+    result = response.json()
+    return result['response']['body']['items']['item']
+
+def parse_weather_items(response):
+    items = response_to_json(response)
+    result = {}
+    for item in items:
+        category = item['category']
+        val = item['fcstValue']
+        result[category] = val
+    return result
 
 if __name__ == "__main__":
     val = get_weather_auto()
-    
+    print(val)
+    print(val.text)
+    print(parse_weather_items(val))
