b596e72deb82f90cc281ad183200c66e67043508
rusestudio <59683457+rusestudio@users.noreply.github.com>
2025-05-16 20:50:16 +0900

    fix json load path

--- src/gemini_suggest_dummy.py
+++ src/gemini_suggest_dummy.py
@@ -17,10 +17,10 @@ weather_data ={
 
 #clothes data
 # Load clothes data from files
-with open("all_clothing_types.json", "r") as f1:
+with open("../clothes_data/all_clothing_types.json", "r") as f1:
     clothing_types = json.load(f1)
 
-with open("all_material_data.json", "r") as f2:
+with open("../clothes_data/all_material_data.json", "r") as f2:
     material_data = json.load(f2)
 
 # Combine into a structured dictionary
