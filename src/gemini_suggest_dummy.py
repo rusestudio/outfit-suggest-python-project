0f4086f6cc88f27311e72d855fb060e8e3a00a18
rusestudio <59683457+rusestudio@users.noreply.github.com>
2025-05-16 21:53:13 +0900

    add generate image

--- src/gemini_suggest_dummy.py
+++ src/gemini_suggest_dummy.py
@@ -1,8 +1,16 @@
 from google import genai
 import json
 import google.generativeai as genai
+import re
+import requests
+import base64
 
+#gemini text api define
 genai.configure(api_key="AIzaSyC8YOsoIj5YuWex1muFSwXCGwcDOaAUUAY")
+
+#api define picture
+api_key ="sk-JPMYX9EfEeL81uMolAfYeIjITWe2XORs0HuGtCkyPXFZwhT9"
+api_url ="https://api.stability.ai/v2beta/stable-image/generate/sd3"
 
 
 #weather data, dummy test data
@@ -58,7 +66,7 @@ def build_prompt(weather_data, clothes_data):
 
         return prompt
 
-
+#send prompt to gemini
 def get_result(prompt: str):
     model=genai.GenerativeModel("gemini-2.0-flash")
     response = model.generate_content(prompt)
@@ -72,3 +80,44 @@ prompt = build_prompt(weather_data, clothes_data)
 result = get_result(prompt)
 
 print(result)
+
+#def return image prompt only
+def image_prompt(result):
+     imageprompts = re.findall(r'\*\*Image Prompt:\*\*\s*"([^"]+)"', result)
+    
+     return imageprompts
+
+#prompt_text
+
+imageprompts =image_prompt(result)
+
+joined_prompts =";".join(imageprompts) if imageprompts else "fashion outfits"
+
+prompt_text = f"generate 3 outfit in one picture based on the {joined_prompts}"
+
+#pay load
+files = {
+    "prompt" : (None, prompt_text),
+    "output_format": (None, "png"),
+}
+
+#header
+headers = {
+    "Authorization": f"Bearer {api_key}",
+    "Accept": "application/json",
+}
+
+#send api
+response = requests.post(api_url, files=files, headers=headers)
+
+#exception handle
+if response.status_code == 200:
+    image_data = response.json()["image"]
+
+    #save image
+    with open("generated_image.png", "wb") as f:
+        f.write(base64.b64decode(image_data))
+    print("image saved")
+else:
+    print(f" Error: {response.status_code} - {response.text}")
+
