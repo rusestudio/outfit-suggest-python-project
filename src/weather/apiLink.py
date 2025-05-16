d9894fe487f7d4df812ea720887d22817ee6dd93
Lkyumin <kyumin4959@gmail.com>
2025-05-16 17:23:34 +0900

    Update apiLink.py

--- src/weather/apiLink.py
+++ src/weather/apiLink.py
@@ -1,45 +1,46 @@
 import requests
 import datetime
 import json
-from typing import List
+from get_data import get_location
+from typing import Tuple
 
 # get weather from API
-def get_weather( day : int, time : int, pos : List[int,int]) -> List:
+def get_weather_manual( pos : Tuple[float,float], date : int, time : int 
+                       , number_of_rows : int = 10, page_number : int = 1 
+                       , data_type : str = 'JSON', day : int = 0 ):
     # key and url
     with open(".key/.sevicekey_decode",'r') as f:
         service_key = f.read()
     url = "http://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/getVilageFcst"
-
-    # user data
-    num_of_rows = 10
-    date_int = int(datetime.datetime.now().strftime("%Y%m%d%H%M"))
-    corrent_time = (date_int%10000)/100
-    corrent_time = int(corrent_time - ((corrent_time-2)%3))%24
-    corrent_time = f'{corrent_time:02d}00'
-
-    # weather format
-    data_type = "JSON"
-    page_no = 1
-
-    # parameters for requests.get()
+ 
     params = {
         'serviceKey' : service_key,
-        'numOfRows' : num_of_rows,
-        'pageNo' : 1,
+        'numOfRows' : number_of_rows,
+        'pageNo' : page_number,
         'dataType' : data_type,
-        'base_date' : int(date_int/10000) + day,
-        'base_time' : corrent_time,
+        'base_date' : date + day,
+        'base_time' : time,
         'nx' : pos[0],
         'ny' : pos[1]
     }
+
     return requests.get(url,params)
 
-def get_posision() -> List[float,float]:
-    with open(".key/.google_key",'r') as f:
-        service_key = f.read()
-    url = f'https://www.googleapis.com/geolocation/v1/geolocate?key={service_key}'
+def get_weather_auto( day : int = 0 ):
+    pos = get_location()
     
-    response = requests.post(url)['location']
-    lat = round(response['lat'], 2)
-    lng = round(response['lng'], 2)
-    return [lat,lng]
+    num_of_rows = 10
+    
+    date_int = int(datetime.datetime.now().strftime("%Y%m%d%H%M"))
+    
+    corrent_time = (date_int%10000)/100
+    corrent_time = int(corrent_time - ((corrent_time-2)%3))%24
+    corrent_time = f'{corrent_time:02d}00'
+    
+    data_type = "JSON"
+
+    page_no = 1
+    return get_weather_manual( pos, date_int, corrent_time )
+
+if __name__ == __main__:
+    get_weather_auto()
