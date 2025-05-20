2a7ea0c8033b27c6d7383a6161d798ef801a69e7
rusestudio <59683457+rusestudio@users.noreply.github.com>
2025-05-20 23:49:38 +0900

    edit main function

--- src/llm_model_suggest.py
+++ src/llm_model_suggest.py
@@ -4,7 +4,6 @@ import re
 
 import google.generativeai as genai
 import requests
-from google import genai
 
 from data_to_be_prompt import clothes_data, user_data, weather_data
 from prompt import build_prompt, image_prompt
@@ -21,44 +20,46 @@ api_url = "https://api.stability.ai/v2beta/stable-image/generate/sd3"
 def get_result(prompt: str):
     model = genai.GenerativeModel("gemini-2.0-flash")
     response = model.generate_content(prompt)
-
     return response.text
 
+def main():
+    # call def build prompt
+    prompt = build_prompt(user_data, weather_data, clothes_data)
 
-# call def build prompt
-prompt = build_prompt(user_data, weather_data, clothes_data)
+    # call gemini
+    result = get_result(prompt)
+    print(result)
 
-# call gemini
-result = get_result(prompt)
+    # prompt_text
+    imageprompts = image_prompt(result)
 
-print(result)
-
-# prompt_text
-imageprompts = image_prompt(result)
-
-# header
-headers = {
-    "Authorization": f"Bearer {api_key}",
-    "Accept": "application/json",
-}
-
-for n, prompt_text in enumerate(imageprompts, start=1):
-    files = {
-        "prompt": (None, prompt_text),
-        "output_format": (None, "png"),
+    # header
+    headers = {
+        "Authorization": f"Bearer {api_key}",
+        "Accept": "application/json",
     }
-    # send api
-    # make respone in list append then request post
-    response = requests.post(api_url, files=files, headers=headers)
 
-    # exception handle
-    if response.status_code == 200:
-        image_data = response.json()["image"]
+    for n, prompt_text in enumerate(imageprompts, start=1):
+        files = {
+            "prompt": (None, prompt_text),
+            "output_format": (None, "png"),
+        }
+        # send api
+        # make respone in list append then request post
+        response = requests.post(api_url, files=files, headers=headers)
 
-        # save image
-        filename = f"../generated_img_data/generated_image{n}.png"
-        with open(filename, "wb") as f:
-            f.write(base64.b64decode(image_data))
-        print(f" {n} image saved")
-    else:
-        print(f" Error:image {n}, {response.status_code} - {response.text}")
+        # exception handle
+        if response.status_code == 200:
+            image_data = response.json()["image"]
+
+            # save image
+            filename = f"../generated_img_data/generated_image{n}.png"
+            with open(filename, "wb") as f:
+                f.write(base64.b64decode(image_data))
+            print(f" {n} image saved")
+        else:
+            print(f" Error:image {n}, {response.status_code} - {response.text}")
+
+# Only run this if the file is executed directly
+if __name__ == "__main__":
+    main()
