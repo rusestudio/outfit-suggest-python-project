6482082cd1ce9a295cabd8f8078dc8a18da70fdf
Lkyumin <kyumin4959@gmail.com>
2025-05-27 23:49:47 +0900

    more fast

--- weather/get_data.py
+++ weather/get_data.py
@@ -1,5 +1,6 @@
 from typing import Tuple
-import math
+from math import pi, tan, log, cos, pow, sin, radians, atan
+from numba import njit
 
 # Variable for converting from lat,lon to grid
 RAD = 6371.00877 # Earth's radius
@@ -11,40 +12,54 @@ OLAT = 38.0      # Reference lat (degrees)
 XO = 43          # Reference X cordinate
 YO = 136         # Reference Y cordinate
 
-# This function should get location infomation
-# TODO
-def get_location() -> Tuple[float,float]:
-    return [37.5665,126.9780]
+# Precomputed values
+DEGRAD = pi / 180.0
+re = RAD / GRID
+slat1 = SLAT1 * DEGRAD
+slat2 = SLAT2 * DEGRAD
+olon = OLON * DEGRAD
+olat = OLAT * DEGRAD
+sn = tan(pi * 0.25 + slat2 * 0.5) / tan(pi * 0.25 + slat1 * 0.5)
+sn = log(cos(slat1) / cos(slat2)) / log(sn)
+sf = tan(pi * 0.25 + slat1 * 0.5)
+sf = pow(sf, sn) * cos(slat1) / sn
+ro = tan(pi * 0.25 + olat * 0.5)
+ro = re * sf / pow(ro, sn)
 
-def latlon_to_grid( pos : Tuple[float,float] ) -> Tuple[int,int]:
-    DEGRAD = math.pi / 180.0
-
-    re = RAD / GRID
-    slat1 = SLAT1 * DEGRAD
-    slat2 = SLAT2 * DEGRAD
-    olon = OLON * DEGRAD
-    olat = OLAT * DEGRAD
-
-    sn = math.tan(math.pi * 0.25 + slat2 * 0.5) / math.tan(math.pi * 0.25 + slat1 * 0.5)
-    sn = math.log(math.cos(slat1) / math.cos(slat2)) / math.log(sn)
-    sf = math.tan(math.pi * 0.25 + slat1 * 0.5)
-    sf = math.pow(sf, sn) * math.cos(slat1) / sn
-    ro = math.tan(math.pi * 0.25 + olat * 0.5)
-    ro = re * sf / math.pow(ro, sn)
-
-    ra = math.tan(math.pi * 0.25 + (pos[0]) * DEGRAD * 0.5)
-    ra = re * sf / math.pow(ra, sn)
-    theta = pos[1] * DEGRAD - olon
-    if theta > math.pi:
-        theta -= 2.0 * math.pi
-    if theta < -math.pi:
-        theta += 2.0 * math.pi
+def latlon_to_grid(lat: float, lon: float,
+                   re: float, sf: float, sn: float, ro: float,
+                   XO: float, YO: float, olon: float) -> Tuple[int, int]:
+    ra = tan(pi * 0.25 + lat * DEGRAD * 0.5)
+    ra = re * sf / pow(ra, sn)
+    theta = lon * DEGRAD - olon
+    if theta > pi:
+        theta -= 2.0 * pi
+    if theta < -pi:
+        theta += 2.0 * pi
     theta *= sn
 
-    x = int(ra * math.sin(theta) + XO + 0.5)
-    y = int(ro - ra * math.cos(theta) + YO + 0.5)
+    x = int(ra * sin(theta) + XO + 0.5)
+    y = int(ro - ra * cos(theta) + YO + 0.5)
 
     return [x, y]
 
+def combert_latlon_to_grid( lat, lon ) -> Tuple[int,int]:
+    '''
+    Change latitude and longitude to Korean weather grid coordinates.
+
+    Param:
+        pos : Tuple[float, float]
+            Index[0] : latitude
+            Index[1] : longitude
+
+    return:
+        grid : Tuple[int, int]
+            Index[0] : grid xpos
+            Index[1] : grid ypos
+    '''
+    return latlon_to_grid(lat,lon,re,sf,sn,ro,XO,YO,olon)
+
+    
+
 if __name__ == "__main__":
-    print(latlon_to_grid([37.5665,126.9780]))
+    print(combert_latlon_to_grid([37.5665,126.9780]))
