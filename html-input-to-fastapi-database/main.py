1660f2853f05cdbea8671eef9c6f99dcf8a5b759
rusestudio <59683457+rusestudio@users.noreply.github.com>
2025-06-06 15:01:08 +0900

    sign up

--- html-input-to-fastapi-database/main.py
+++ html-input-to-fastapi-database/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI, Request
 from pydantic import BaseModel
 from fastapi.templating import Jinja2Templates
+from typing import List
 
 app = FastAPI()
 
@@ -10,23 +11,34 @@ templates = Jinja2Templates(directory="templates")
 #dummy pydantic data
 #will be implement as sqlmodel by 용한님님
 class loginData(BaseModel):
-    id: str
-    userid: str
+    user_id: str
+    password: str
+
+class SignUp(BaseModel):
+    user_id: str
     password: str
     sex: str
     age: int
     height: float
     weight: float
-    body_temp: str
+    body_temp: int
+    clothes: List[str]
 
 
 @app.get("/")
 def get_page(request: Request):
-    return templates.TemplateResponse("index.html", {"request":request})
+    return templates.TemplateResponse("signup.html", {"request":request})
 
 #사용자에게 입력데이터 database으로 저장할 수있게게
 @app.post("/submit")
-async def submit_form(data: loginData):
+async def submit_form(data: SignUp):
     #save the data to the database 용한님 refer
+    print("Received signup data:", data)
     return {"message": "data received!", 
             "data": data}
+
+#/login
+@app.get("/login")
+def get_login(request: Request):
+    return templates.TemplateResponse("login.html", {"request": request})
+#/dashboard
